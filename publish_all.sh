9eac0d3d4e20301355762c54905fead86c5ac776
Vincent Lee <vincent@akaun.com>
2022-05-15 06:31:01 +0800

    many changes

---
+++ publish_all.sh
@@ -0,0 +1,7 @@
+asciidoctor -o docs/index.html all_in_one.adoc
+asciidoctor -b docbook5 -o docs/all_in_one.xml all_in_one.adoc
+asciidoctor -o docs/all_in_one-$(date +%Y-%m-%d).html all_in_one.adoc
+asciidoctor -b docbook5 -o docs/all_in_one-$(date +%Y-%m-%d).xml all_in_one.adoc
+asciidoctor -r asciidoctor-pdf -b pdf -o docs/all_in_one.pdf all_in_one.adoc
+# asciidoctor -r asciidoctor-epub3 -b epub3 -o docs/all_in_one.epub all_in_one.adoc
+# asciidoctor-epub3 -o docs/all_in_one.epub all_in_one.adoc
