73116ec5257a8c6fb5cc62ffb5ab1af8c77850c4
Vincent Lee <vincent@akaun.com>
2022-05-18 02:22:06 +0800

    Compiled and commit ..

--- bin/macos_install.sh
+++ bin/macos_install.sh
@@ -1,3 +1,4 @@
+## Installing Homebrew - 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
