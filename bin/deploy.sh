19a07d0ab4961894599633765a86ec2beebf60e5
Vincent Lee <vincent@akaun.com>
2022-08-06 15:19:41 +0800

    removed pdf epub xml compilation from build and deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -23,21 +23,21 @@ asciidoctor --trace -r asciidoctor-multipage -b multipage_html5 -o index.html -D
 echo 'Compiling single file.html....'
 asciidoctor -o build/blg_handbook/www/blg_handbook.html tmp/index.adoc
 
-echo 'Compiling DocBook format....'
-asciidoctor -b docbook5 -o build/www/blg_handbook/blg_handbook.xml tmp/index.adoc
+#echo 'Compiling DocBook format....'
+#asciidoctor -b docbook5 -o build/www/blg_handbook/blg_handbook.xml tmp/index.adoc
 
-echo 'Compiling PDF Format....'
-asciidoctor --trace -r asciidoctor-pdf -b pdf -o build/blg_handbook/www/blg_handbook.pdf tmp/index.adoc
+#echo 'Compiling PDF Format....'
+#asciidoctor --trace -r asciidoctor-pdf -b pdf -o build/blg_handbook/www/blg_handbook.pdf tmp/index.adoc
 # asciidoctor -a route-style=monokai --trace -r asciidoctor-pdf -b pdf -o build/all_in_one/all_in_one.pdf content/books/all_in_one/index.adoc
 
-echo 'Compiling EPUB Format .....'
-asciidoctor-epub3 -o build/blg_handbook/www/blg_handbook.epub tmp/index.adoc
+#echo 'Compiling EPUB Format .....'
+#asciidoctor-epub3 -o build/blg_handbook/www/blg_handbook.epub tmp/index.adoc
 # asciidoctor -r asciidoctor-epub3 -b epub3 -o docs/all_in_one.epub pub/all_in_one.adoc
 
 
 echo 'Copying docbook, pdf, epub back to the "attachments" folder ...'
 #cp tmp/blg_handbook* content/chapters/User_Guide/modules/general-users/attachments 
-cp build/blg_handbook/www/blg_handbook* content/chapters/User_Guide/modules/general-users/attachments 
+#cp build/blg_handbook/www/blg_handbook* content/chapters/User_Guide/modules/general-users/attachments 
 
 echo 'Copying files to docs folder for github pages publishing .... '
 cp -r build/blg_handbook/www/* docs
